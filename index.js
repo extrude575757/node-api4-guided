48fc500c494a368305f0140d3302242b750282c2
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-20 20:24:04 -0800

    Heroku auth:login

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const server = require("./api/server.js");
-// const port = process.env.PORT;
+const port = process.env.PORT;
 // const port = 5000;
+// Use heroku auth:login
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
